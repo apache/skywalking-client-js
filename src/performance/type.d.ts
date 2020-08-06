d719e51fb085f16585bf21fbd526ddd899137ca5
Qiuxia Fan <fine0830@outlook.com>
2020-08-06 17:22:24 +0800

    style: add notes

--- src/performance/type.d.ts
+++ src/performance/type.d.ts
@@ -26,18 +26,18 @@ export type Els = Array<{
   weight: number;
 }>;
 export type IPerfDetail = {
-  redirectTime: number | undefined;
-  dnsTime: number | undefined;
+  redirectTime: number | undefined; // Time of redirection
+  dnsTime: number | undefined; // DNS query time
   ttfbTime: number | undefined; // Time to First Byte
-  tcpTime: number | undefined;
-  transTime: number | undefined;
-  domAnalysisTime: number | undefined;
+  tcpTime: number | undefined; // tcp connection time
+  transTime: number | undefined; // Content transfer time
+  domAnalysisTime: number | undefined; // Dom parsing time
   fptTime: number | undefined; // First Paint Time or Blank Screen Time
-  domReadyTime: number | undefined;
+  domReadyTime: number | undefined; // Dom ready time
   loadPage: number | undefined; // Page full load time
   resTime: number | undefined; // Synchronous load resources in the page
   sslTime: number | undefined; // Only valid for HTTPS
-  ttlTime: number | undefined; // time to interact
+  ttlTime: number | undefined; // Time to interact
   firstPackTime: number | undefined; // first pack time
   fmpTime: number | undefined; // First Meaningful Paint
 };
