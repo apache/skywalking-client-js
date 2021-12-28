b32f9f00a84ba0f74faae2165964ec50c61677ea
tickli <lizhen55521@gmail.com>
2021-12-28 09:48:48 +0800

    feat: change SpanLayer type and SpanType type according to tracing pb (#76)
    Co-authored-by: tickli <tickli@tencent.com>

--- src/trace/type.d.ts
+++ src/trace/type.d.ts
@@ -28,8 +28,8 @@ export interface SpanFields {
   startTime: number;
   endTime: number;
   spanId: number;
-  spanLayer: string;
-  spanType: string;
+  spanLayer: number;
+  spanType: number;
   isError: boolean;
   parentSpanId: number;
   componentId: number;
