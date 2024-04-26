913e6d43943dd1750e1f677ad97285822c0caa7e
Fine0830 <fanxue0830@gmail.com>
2024-04-26 10:44:28 +0800

    feat: add listener and observer

---
+++ src/services/observe.ts
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+interface PerformanceEntryObj {
+  'largest-contentful-paint': any[];
+}
+
+export function observe <K extends keyof PerformanceEntryObj>(
+  type: K,
+  callback: (entries: PerformanceEntryObj[K]) => void,
+  opts?: PerformanceObserverInit,
+): PerformanceObserver {
+  try {
+    if (PerformanceObserver.supportedEntryTypes.includes(type)) {
+      const perfObs = new PerformanceObserver((list) => {
+
+        Promise.resolve().then(() => {
+          callback(list.getEntries() as PerformanceEntryObj[K]);
+        });
+      });
+      perfObs.observe(
+        Object.assign(
+          {
+            type,
+            buffered: true,
+          },
+          opts || {},
+        ) as PerformanceObserverInit,
+      );
+      return perfObs;
+    }
+  } catch (e) {
+    console.error(e);
+  }
+};
--- src/services/prerenderListener.ts
+++ src/services/prerenderListener.ts
@@ -14,32 +14,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export enum ErrorsCategory {
-  AJAX_ERROR = 'ajax',
-  RESOURCE_ERROR = 'resource',
-  VUE_ERROR = 'vue',
-  PROMISE_ERROR = 'promise',
-  JS_ERROR = 'js',
-  UNKNOWN_ERROR = 'unknown',
+export function watchPrerendering(callback: () => void) {
+  if ((document as any).prerendering) {
+    addEventListener('prerenderingchange', callback, true);
+  } else {
+    callback();
+  }
 }
-export enum GradeTypeEnum {
-  INFO = 'Info',
-  WARNING = 'Warning',
-  ERROR = 'Error',
-}
-export enum ReportTypes {
-  ERROR = '/browser/errorLog',
-  ERRORS = '/browser/errorLogs',
-  PERF = '/browser/perfData',
-  SEGMENT = '/v3/segment',
-  SEGMENTS = '/v3/segments',
-}
-
-export const SpanLayer = 'Http';
-export const SpanType = 'Exit';
-
-export enum ReadyStatus {
-  OPENED = 1,
-  DONE = 4,
-}
-export const ComponentId = 10001; // ajax
