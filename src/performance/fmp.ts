f11b6c78529f2bffbba0f4388615d169d1f9d787
Fine0830 <fanxue0830@gmail.com>
2025-02-07 10:45:13 +0800

    update client test

--- src/performance/fmp.ts
+++ src/performance/fmp.ts
@@ -115,8 +115,7 @@ class FMPTiming {
         return false;
       }
       const resultEls: ElementList = this.filterResult(tp.els);
-      const fmpTiming: number = this.getFmpTime(resultEls);
-      this.fmpTime = fmpTiming;
+      this.fmpTime = this.getFmpTime(resultEls);
     } else {
       setTimeout(() => {
         this.calculateFinalScore();
